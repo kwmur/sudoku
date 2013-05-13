a2afaaeb309df669d7ada7e9b810a6c3a45161e2
user <you@example.com>
2013-05-13 19:28:18 +0900

    add make cell func

--- sudoku.php
+++ sudoku.php
@@ -11,10 +11,16 @@ function makeCells()
   $cells = array();
   for ($i = 0; $i < 10; ++$i) {
     $cells[] = array();
+    for ($j = 0; $j < 10; ++$j) {
+      #$cells[$i][$j] = mt_rand(0, 9);
+      $cells[$i][$j] = mt_rand(0, 1) ? mt_rand(0, 9) : null;
+    }
   }
+
+  return $cells;
 }
 
-print_r($cells);
+print_r(makeCells());
 
 
 
